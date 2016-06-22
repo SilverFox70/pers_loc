df74d71b537a92155ff25f1dbcd5c1acf59d047d
SilverFox70 <wfbrinkert@gmail.com>
2016-06-22 14:45:25 -0700

    Inflate db

--- db/schema.rb
+++ db/schema.rb
@@ -49,34 +49,6 @@ ActiveRecord::Schema.define(version: 20160318043324) do
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
 
-  create_table "sessions", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "phone_number"
-    t.boolean  "leave_message"
-    t.text     "message_explanation"
-    t.string   "email"
-    t.string   "email_confirm"
-    t.integer  "age"
-    t.text     "how_found"
-    t.date     "date"
-    t.time     "time"
-    t.text     "why_session"
-    t.string   "reference_name"
-    t.string   "reference_email"
-    t.string   "reference_phone"
-    t.string   "reference_link"
-    t.text     "reference_identifier"
-    t.string   "social_link_1"
-    t.string   "social_link_2"
-    t.boolean  "service_agreement"
-    t.boolean  "cancellation_policy"
-    t.string   "spam_question"
-    t.boolean  "location_aware"
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
